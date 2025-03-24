34c7a0e87684f8e7eff3814e8a5571ddaaf4fe9e
Josué Medino <josuemedino5@gmail.com>
2025-03-24 01:41:02 -0300

    Exibir caminho e valores importantes

--- path.cpp
+++ path.cpp
@@ -3,10 +3,12 @@
 #include <fstream>
 #include <stack>
 #include <set>
-
+#include "readInstances.h"
 using namespace std;
 
 int main() {
+    readInstances::DataOP data = readInstances::readFile("./instancias/quality/instances/berlin52FSTCII_q2_g4_p40_r20_s20_rs15.pop");
+
     ifstream arquivo("matriz_Xij.txt");
 
     if (!arquivo) {
@@ -60,11 +62,18 @@ int main() {
 
     // Exibindo o caminho encontrado
     cout << "Caminho gerado: ";
+    double prize = 0, cost = 0;
     for (size_t i = 0; i < caminho.size(); i++) {
         cout << caminho[i];
-        if (i < caminho.size() - 1) cout << " -> ";
+        prize += data.prize[caminho[i]];
+        if (i < caminho.size() - 1) {
+            cout << " -> ";
+            cost += data.cost[caminho[i]][caminho[i + 1]];
+        }
     }
     cout << endl;
-
+    cout << "Prize: " << prize << endl;
+    cout << "Cost: " << cost << endl;
+    cout << "Objective function: " << prize - 0.001*cost << endl;
     return 0;
 }
