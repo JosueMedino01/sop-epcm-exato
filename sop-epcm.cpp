eea42cd62b620806169246a47347c5f5977d1bea
Josué Medino <josuemedino5@gmail.com>
2025-08-05 10:22:51 -0300

    feat: add subtour cut

--- sop-epcm.cpp
+++ sop-epcm.cpp
@@ -9,9 +9,8 @@ using namespace std;
 
 const string PATH = "./instancias/quality/instances/berlin52FSTCII_q2_g4_p40_r20_s20_rs15.pop";
 const int startNode = 0;
-const int endNode = 0;
-const double minPrize = 1000;
-
+const int endNode = 14;
+const double minPrize = 6000;
 
 readInstances::DataOP getInstance(string path) {
     return readInstances::readFile(path);
@@ -19,7 +18,7 @@ readInstances::DataOP getInstance(string path) {
 
 IloInt checkTour(IloNumArray2 Xij, IloNumArray seen, IloNum tol)
 {
-	IloInt j, n	 = Xij.getSize();
+	IloInt j = 0, n	 = Xij.getSize();
 	IloInt last	 = -1;
 	IloInt length  = 0;
 	IloInt current = 0;
@@ -27,28 +26,34 @@ IloInt checkTour(IloNumArray2 Xij, IloNumArray seen, IloNum tol)
 	seen.add(n, 0.0);
 
 	// Search for a subtour if sol[] is integer feasible
+	while (seen[current] == 0)
+    {
+        length++;
+        seen[current] = length;
+        IloInt next = -1;
 
-	while ( seen[current] == 0 )
-	{
-		length++;
-		seen[current] = length;
-		for (j = 0; j < current; j++)
-		{
-			if ( j != last  &&  Xij[current][j] >= 1.0-tol ) break;
-		}
-		if ( j == current )
-		{
-			for (j = current+1; j < n; j++) {
-				if ( j != last  &&  Xij[j][current] >= 1.0-tol ) break;
-			}
-		}
-		if ( j == n ) return (n+1);
-		last = current;
-		current = j;
-	}
+        for (j = 0; j < n; j++)
+        {
+            if (Xij[current][j] >= 1.0 - tol) {
+                if (j != last || (j == startNode && length > 1)) {
+                    next = j;
+                    break;
+                }
+            }
+        }
 
-	return (length);
+        if (next == -1) {
+            cout << "NOT CYCLE" << endl;
+            return n + 1;
+        }
+
+        last = current;
+        current = next;
+    }
+
+    return length;
 }
+
 
 ILOLAZYCONSTRAINTCALLBACK2(SubtourEliminationCallback, IloArray<IloIntVarArray>, Xij, IloNum, tol) {
     IloInt n = Xij.getSize();
@@ -57,9 +62,14 @@ ILOLAZYCONSTRAINTCALLBACK2(SubtourEliminationCallback, IloArray<IloIntVarArray>,
 
     for (int i = 0; i < n; i++)
     {
-        sol[i] = IloNumArray(env);
-        getValues(sol[i], Xij[i]);
+        sol[i] = IloNumArray(env, n);
+        for (int j = 0; j < n; j++) {
+            if(i != j){
+                sol[i][j] = getValue(Xij[i][j]);
+            }
+        }
     }
+   
     
     IloNumArray seen(env);
     IloInt length = checkTour(sol, seen, tol);
@@ -73,25 +83,23 @@ ILOLAZYCONSTRAINTCALLBACK2(SubtourEliminationCallback, IloArray<IloIntVarArray>,
         }
 
         sol.end();
+        cout << "NOT CYCLE" << endl;
         return;
     }
 
     IloExpr subtour(env);
-    for (int i = 0; i < n; i++)
-    {
-        if(seen[i])
-        {
-            for (int j = i + 1; j < n; j++)
-            {
-                if(seen[j]) {
+    for (int i = 0; i < n; i++) {
+        if (seen[i]) {
+            for (int j = 0; j < n; j++) {
+                if (i != j && seen[j]) {
                     subtour += Xij[i][j];
                 }
             }
-            
         }
     }
-    
-    add(subtour <= length - 1).end();
+   
+    add((subtour <= length - 1)).end();
+
     subtour.end();
     seen.end();
 
@@ -106,9 +114,9 @@ ILOLAZYCONSTRAINTCALLBACK2(SubtourEliminationCallback, IloArray<IloIntVarArray>,
 
 
 
-/* static std::map<std::pair<int, double>, double> hashMap; */
+static std::map<std::pair<int, double>, double> hashMap;
 
-/* double evaluate(int i, double Pmin, const vector<int> &path, 
+double evaluate(int i, double Pmin, const vector<int> &path, 
     const vector<double> &probabilities, const vector<int> &prizes) 
 {
     if (Pmin <= 0) 
@@ -133,104 +141,31 @@ ILOLAZYCONSTRAINTCALLBACK2(SubtourEliminationCallback, IloArray<IloIntVarArray>,
     hashMap[{i, Pmin}] = prob;
 
     return prob;
-} */
-
-/* struct EvaluateTourProb : IloCplex::LazyConstraintCallbackI {
-    IloEnv& env;
-    IloArray<IloIntVarArray>& Xij;
-    readInstances::DataOP data;
-
-    EvaluateTourProb(IloEnv& env, IloArray<IloIntVarArray> &Xij, readInstances::DataOP data) :
-        LazyConstraintCallbackI{env}, env{env}, Xij{Xij}, data{data} {}
-
-    [[nodiscard]] IloCplex::CallbackI* duplicateCallback() const override {
-        return new(env) EvaluateTourProb{*this};
-    }
-
-    void main() override {
-                vector<int> path;
-        vector<bool> visited(data.nCustomers, false);
-        int current = startNode;
-        path.push_back(current);
-        visited[current] = true;
-
-        while (current != endNode) {
-            bool found = false;
-            for (int j = 0; j < data.nCustomers; j++) {
-                if (current != j && !visited[j] && getValue(Xij[current][j]) == 1) {
-                    path.push_back(j);
-                    visited[j] = true;
-                    current = j;
-                    found = true;
-                    break;
-                }
-            }
-        }
-
-        double prob = evaluate(path.size(), minPrize, path, data.probability,  data.prize);
-      
-        
-
-        IloExpr lazyConstraint(env);
-        
-        add(lazyConstraint < 1);
-        lazyConstraint.end();
-    };
-}; */
-
-
-
-
-/* bool dfsCheck(int node, vector<int> adj[], int vis[], int pathVis[]){ 
-    vis[node] = 1;
-    pathVis[node] = 1;
-
-    for (auto it : adj[node]) 
-    {
-        if(!vis[it]) {
-            if(dfsCheck(it, adj, vis, pathVis) == true)
-                return true;
-        }
-        else if(pathVis[it]) {
-            return true;
-        }
-    }
-
-    pathVis[node] = 0;
-    return false;
 }
 
-
-bool isCyclic(int V, vector<int> adj[]) {
-    int vis[V] = {0};
-    int pathVis[V] = {};
-
-    for (int i = 0; i < V; i++)
-    {
-        if(!vis[i]) {
-            if(dfsCheck(i, adj, vis, pathVis) == true) return true;
-        }
-    }
-    
-    return false;
-}
- */
 #include <fstream>
 
-void writeResponse(readInstances::DataOP &data, IloCplex &cplex, IloArray<IloIntVarArray> &Xij) {
+void writeResponse(readInstances::DataOP &data, IloCplex &cplex, IloArray<IloIntVarArray> &Xij, IloIntVarArray &Yi) {
     double sumPrizes = 0;
-    if (cplex.solve()) {
+    if (cplex.getStatus() == IloAlgorithm::Optimal ||
+    cplex.getStatus() == IloAlgorithm::Feasible) {
         double funcaoObjetivo = cplex.getObjValue(); 
         cout << "Valor da função objetivo: " << funcaoObjetivo << endl;
 
         cout << "Arestas (i, j) ativadas:" << endl;
         for (int i = 0; i < data.nCustomers; i++) {
             for (int j = 0; j < data.nCustomers; j++) {
-                if (i != j && cplex.getValue(Xij[i][j]) > 0.5) {
-                    cout << "(" << i + 1  << ", " << j + 1 << ") ";
-                    sumPrizes += data.prize[j];
+                if (i != j && cplex.getValue(Xij[i][j]) > 0) {
+                    cout << "(" << i + 1  << ", " << j + 1 << ")" << endl;
                 }
-            }
+            }   
+        }
+        cout << endl;
+        for (int i = 0; i < data.nCustomers; i++) {
+            if(cplex.getValue(Yi[i]) >= 1) {
+                sumPrizes += data.prize[i];
+                cout << i + 1 << ": " << cplex.getValue(Yi[i]) << endl;
+            } 
         }
         cout << endl;
 
@@ -267,7 +202,6 @@ void solver(readInstances::DataOP &data) {
         Xij[i] = IloIntVarArray(environment, n, 0, 1);
 
     IloIntVarArray Yi(environment, n, 0, 1);
-    /* IloIntVarArray U(environment, n, 1, n); */
 
     /* FUNÇÃO OBJETIVO */
     IloExpr function(environment);
@@ -294,70 +228,42 @@ void solver(readInstances::DataOP &data) {
         
     POP_RC.add(edgeSumWithEndNode == 1); edgeSumWithEndNode.end();
 
-    // Eliminação de subciclo
-    /* for (int i = 1; i < n; i++)
+    // Se sai aresta de i então i está na solução (Yi = 1)
+    for(int i = 0; i < n; i++)
     {
-        POP_RC.add(U[i] >= 1);
-        POP_RC.add(U[i] <= n - 1);
+        IloExpr edgeOutI(environment);
+        IloExpr edgeOutJ(environment);
+
+        for (int j = 0; j < n; j++)
+        {
+            if(i != j) {
+                edgeOutI += Xij[i][j];
+                edgeOutJ += Xij[j][i];
+            }
+        }
+
+        POP_RC.add(edgeOutI == Yi[i]);
+        POP_RC.add(edgeOutJ == Yi[i]);
+
+        edgeOutI.end();
+        edgeOutJ.end();
     }
-    
-    for (int i = 1; i < n; i++) {
-        for (int j = 1; j < n; j++) {
-            if (i != j) {
-                POP_RC.add(U[i] - U[j] + 1 <= (n - 1) * (1 - Xij[i][j]));
-            }
-        }
-    } */
-
-    /* Garantir conectividade e que é no mínimo visitado uma vez */
-    /* for (int k = 1; k < n - 1; k++) {
-        IloExpr inSum(environment);
-        IloExpr outSum(environment);
-        
-        for (int i = 0; i < n; i++) {
-            if (i != k) {
-                inSum += Xij[i][k];  
-            }
-        }
-    
-        for (int j = 0; j < n; j++) {
-            if (j != k) {
-                outSum += Xij[k][j];
-            }
-        }
-    
-        // Garante que cada nó tenha no máximo uma entrada e uma saída
-        POP_RC.add(inSum <= 1);
-        POP_RC.add(outSum <= 1);
-        POP_RC.add(inSum == outSum); // Conectividade
-    
-        inSum.end();
-        outSum.end();
-    } */
-        
-
-
-
-    /* Eliminação de Subciclos */
-   /*  IloNum tol = cplex.getParam(IloCplex::EpInt);
-    IloCplex::Callback sec = cplex.use( SubtourEliminationCallback(environment, Xij, tol)); */
 
     // Premiação mínima
     IloExpr prizeSum(environment);
     for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            prizeSum += Xij[i][j] * data.prize[j];
+        prizeSum += Yi[i] * data.prize[i];
     
-    POP_RC.add(prizeSum >= minPrize); prizeSum.end();
+    POP_RC.add(prizeSum >= minPrize); 
+    prizeSum.end();
 
-    // Sem laços
-    /* for (int i = 0; i < n; i++) {
-        POP_RC.add(Xij[i][i] == 0);
-    } */
-
+    /* Eliminação de Subciclos */
+    cplex.extract(POP_RC);
+    IloNum tol = cplex.getParam(IloCplex::EpInt);
+    cplex.use(SubtourEliminationCallback(environment, Xij, tol));
 
     cplex.solve();
-    writeResponse(data, cplex, Xij);
+    writeResponse(data, cplex, Xij, Yi);
 }
 
 int main() {
@@ -365,27 +271,3 @@ int main() {
     solver(data);
     return 0;
 }
-
-
-/* class SubtourEliminationCallback : public IloCplex::LazyConstraintCallbackI {
-    IloArray<IloIntVarArray> Xij;
-    int n;
-
-public:
-    SubtourEliminationCallback(IloEnv env, IloArray<IloIntVarArray>& _Xij, int _n)
-        : IloCplex::LazyConstraintCallbackI(env), Xij(_Xij), n(_n) {}
-
-    void main() override {
-        IloExpr lazyConstraint(getEnv());
-        add(lazyConstraint < 1);
-        lazyConstraint.end();
-        return; 
-    }
-
-    
-
-    IloCplex::CallbackI* duplicateCallback() const override {
-        return new (getEnv()) SubtourEliminationCallback(*this);
-    }
-};
- */
